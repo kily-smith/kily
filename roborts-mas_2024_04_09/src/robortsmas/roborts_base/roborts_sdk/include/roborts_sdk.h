e08a717c3a9ef21e9983338c3481e4828d53a6e3
kily <69972594+kily-smith@users.noreply.github.com>
2024-04-09 23:08:21 +0800

    Delete roborts-mas_2024_04_09 directory

--- roborts-mas_2024_04_09/src/robortsmas/roborts_base/roborts_sdk/include/roborts_sdk.h
+++
@@ -1,9 +0,0 @@
-#ifndef ROBORTS_SDK_INCLUDE_SDK_H_
-#define ROBORTS_SDK_INCLUDE_SDK_H_
-
-#include "protocol_content.h"
-#include "dispatch/handle.h"
-#include "dispatch/dispatch.h"
-#include "utilities/log.h"
-
-#endif //ROBORTS_SDK_INCLUDE_SDK_H_
